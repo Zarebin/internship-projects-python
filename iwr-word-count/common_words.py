1b54b54fa89fd54aaff7d2c925eb7fee934bcec5
hr-samsami <hamidreza.samsami@gmail.com>
2022-05-11 17:17:43 +0430

    Refactor the codes and merge all output file to one Excel file

--- iwr-word-count/common_words.py
+++ iwr-word-count/common_words.py
@@ -1,40 +1,51 @@
 import pandas as pd
 import glob
-from hazm import *
+from hazm import word_tokenize
 
 
-def get_label_list(label_list_path):
-    # Load Labels List
-    df = pd.read_excel(label_list_path, engine='openpyxl', sheet_name=0)
-    labels_list = []
-    for i in range(len(df['ClassName'])):
-        labels_list.append(df['ClassName'][i])
-    return labels_list
+# Read the first column of an Excel file
+# Return the list of values in a column
+def read_excel(path, col_index=0, header=None):
+    df = pd.read_excel(path, engine='openpyxl', header=header, sheet_name=0)
+    return df.iloc[:, col_index].astype(str).values.tolist()
 
 
+# Tokenize a sentence and return a list of words with len > 1
 def clean_text(text):
     word_tokens = word_tokenize(text)
-    word_tokens = [w for w in word_tokens if len(w) > 1]
-    return word_tokens
+    return [w for w in word_tokens if len(w) > 1]
 
 
+# Intersection of two Lists
 def intersection(lst1, lst2):
     lst3 = [value for value in lst1 if value in lst2]
     return lst3
 
 
+# Tokenize the sentence and return the length of common words with label_list
 def get_common_words_count(sentence):
+    global label_list
     tokens = clean_text(sentence)
     return len(intersection(tokens, label_list))
 
 
-sentence_list = []
-excel_files = glob.glob('data/*.xlsx')
-label_list = get_label_list("labels_list_V2_100.xlsx")
+if __name__ == "__main__":
 
-for file_path in excel_files:
-    data = pd.read_excel(file_path, engine='openpyxl', header=None, sheet_name=0)
-    sentence_list = data.iloc[:, 0]
-    data[1] = list(map(get_common_words_count, sentence_list))
-    file_name = file_path.split('/')[-1]
-    data.to_excel(f'convert/{file_name}', index=False, header=False)
+    label_path = "labels_list_V2_100.xlsx"
+    in_path = "input/"
+    out_path = "output/"
+    excel_list = []
+    excel_files = glob.glob(f'{in_path}/*.xlsx')
+    label_list = read_excel(label_path, col_index=1, header=0)
+
+    for excel_path in excel_files:
+        sentence_list = read_excel(excel_path)
+        com_word_count_list = list(map(get_common_words_count, sentence_list))
+        dataframe = pd.DataFrame({
+                '0': sentence_list,
+                '1': com_word_count_list
+        })
+        excel_list.append(dataframe)
+
+    excel_merged = pd.concat(excel_list, ignore_index=True)
+    excel_merged.to_excel(f'{out_path}/merge.xlsx', index=False, header=False)
