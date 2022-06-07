ac55f90a033f20b39ef466558a94b9585c075cef
sepideh <sepidehbod.93@gmail.com>
2022-06-07 14:11:40 +0430

    added some changes in sentiment view

--- crowd-server/crowd_server/apps/sentiment/views.py
+++ crowd-server/crowd_server/apps/sentiment/views.py
@@ -3,12 +3,13 @@ from rest_framework import status, viewsets
 from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.viewsets import ModelViewSet,GenericViewSet
 from .models import Question, EvaluatedSentiment
 from .serializers import QuestionSerializer, EvaluatedSentimentSerializer
 
 
 # Create your views here.
-class SentimentAPI(viewsets.ViewSet):
+class SentimentAPI(GenericViewSet):
 
     def get_queryset(self):
         if self.action == "create":
@@ -27,20 +28,22 @@ class SentimentAPI(viewsets.ViewSet):
             return QuestionSerializer
 
     def retrieve(self, request, pk=None):
-        queryset = Question.objects.filter(number_of_answers__lt=3)
+        queryset = self.get_queryset()
         question = get_object_or_404(queryset, pk=pk)
-        serializer = QuestionSerializer(question)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        serializer = self.get_serializer(question)
+        response = Response(serializer.data, status=status.HTTP_200_OK)
+
+        return response
 
     def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
+        serializer = self.get_serializer(data= request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-    def list(self, request):
-        queryset = Question.objects.all()
-        serializer = QuestionSerializer(queryset, many=True)
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
 
 
