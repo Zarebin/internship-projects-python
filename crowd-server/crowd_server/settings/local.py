40fc6b269aa1a7a1e1e3e0e2ef1f0963cb12d5f8
elnazomman <elnazomman@yahoo.com>
2022-06-12 00:09:10 +0430

    create food_label api

--- crowd-server/crowd_server/settings/local.py
+++ crowd-server/crowd_server/settings/local.py
@@ -1,13 +1,13 @@
 from .base import *
 
 # Database
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'food',
-#         'USER': 'root',
-#         'PASSWORD': 'root',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'food',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
