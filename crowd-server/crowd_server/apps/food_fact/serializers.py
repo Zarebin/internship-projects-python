edd16525fcca3d6f12cd9e4f707003ad434247d4
pashapoor <sasukepashapoor@gmail.com>
2022-06-12 13:59:31 +0430

    change all serialiser to serializer

--- crowd-server/crowd_server/apps/food_fact/serializers.py
+++ crowd-server/crowd_server/apps/food_fact/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from .models import Question,Response
 
 
-class QuestionSerialiser(serializers.ModelSerializer):
+class QuestionSerializer(serializers.ModelSerializer):
 
     class Meta:
     
