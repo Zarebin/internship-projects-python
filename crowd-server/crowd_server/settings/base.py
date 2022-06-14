cdba7c9464f43933847b037c57de3e792f985a48
pashapoor <sasukepashapoor@gmail.com>
2022-06-14 12:12:07 +0430

    add food_fact app to base.py

--- crowd-server/crowd_server/settings/base.py
+++ crowd-server/crowd_server/settings/base.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'crowd_server.apps.sentiment',
     'crowd_server.apps.food_label',
     'rest_framework',
-    'food_fact'
+    'crowd_server.apps.food_fact',  
 ]
 
 MIDDLEWARE = [
