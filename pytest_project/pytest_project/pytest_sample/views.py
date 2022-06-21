5449ba7ff3c3552dab22adee859d317b1932f506
elnazomman <elnazomman@yahoo.com>
2022-06-21 16:11:23 +0430

    A simple Pytest task

---
+++ pytest_project/pytest_project/pytest_sample/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+from django.views.decorators.http import require_http_methods
+
+from .models import PageViewCount
+
+
+@require_http_methods(["GET"])
+def hello_view(request):
+    counter, _ = PageViewCount.objects.get_or_create(title="hello")
+    counter.count += 1
+    counter.save()
+    return HttpResponse(f"Hello world. The counter is: {counter.count}")
+
+
+@require_http_methods(["GET"])
+def goodbye_view(request):
+    return HttpResponse(f"Goodbye world")
